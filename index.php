e90af4119fe5320c1ca4783638dfbedfc48e7d6b
Pablo Fernandez <149872748+Tuthotep@users.noreply.github.com>
2024-11-30 23:05:43 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Redirect to a different page in the current directory that was requested */
-$host  = $_SERVER['HTTP_HOST'];
-$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$extra = 'start.php';
-header("Location: http://$host$uri/inicio/$extra");
-
-
-?>
